9f4147ffd321f812f80dee1fec3e5823529c92b0
zkrakus <zkrakus@gmail.com>
2025-04-10 16:52:54 -0400

    feat:s04-e90 Completed project.

---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,17 @@
+import { CurrencyPipe } from '@angular/common'
+import { Component, computed } from '@angular/core';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [CurrencyPipe],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css'
+})
+export class InvestmentResultsComponent {
+  constructor(private investmentService: InvestmentService){}
+
+  // results = this.investmentService.resultData.asReadonly();
+  results = computed(() => this.investmentService.resultData())
+}
